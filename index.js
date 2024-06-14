8437ab40545605fcc50cbe593b4dbf28b66d7c9d
ARUNKUMARP123 <arun841988@gmail.com>
2024-06-14 21:28:36 +0530

    add origin

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ connectDB();
 const allowedOrigins = [
   "http://localhost:5173",
   "https://main--urlshorten-app.netlify.app",
+  "https://urlshorten-app.netlify.app",
 ];
 
 // CORS configuration
